996bd695acf1c0ee4e8c5ebd7f726a0966695f06
UrayMR <umrajsyafiq@gmail.com>
2026-09-08 18:08:16 +0700

    refactor: clean up unused code and simplify navigation menu structure

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui'
 
-const route = useRoute()
-const toast = useToast()
+// const route = useRoute()
+// const toast = useToast()
 
 const open = ref(false)
 
@@ -13,69 +13,14 @@ const links = [[{
   onSelect: () => {
     open.value = false
   }
-}, {
-  label: 'Customers (Example)',
-  icon: 'i-lucide-users',
-  to: '/customers',
-  onSelect: () => {
-    open.value = false
-  }
-}, {
-  label: 'Users',
-  icon: 'i-lucide-user-round',
-  to: '/users',
-  onSelect: () => {
-    open.value = false
-  }
-}, {
+},
+{
   label: 'Employees',
   icon: 'i-lucide-briefcase-business',
   to: '/employees',
   onSelect: () => {
     open.value = false
   }
-}, {
-  label: 'Settings',
-  to: '/settings',
-  icon: 'i-lucide-settings',
-  defaultOpen: true,
-  type: 'trigger',
-  children: [{
-    label: 'General',
-    to: '/settings',
-    exact: true,
-    onSelect: () => {
-      open.value = false
-    }
-  }, {
-    label: 'Members',
-    to: '/settings/members',
-    onSelect: () => {
-      open.value = false
-    }
-  }, {
-    label: 'Notifications',
-    to: '/settings/notifications',
-    onSelect: () => {
-      open.value = false
-    }
-  }, {
-    label: 'Security',
-    to: '/settings/security',
-    onSelect: () => {
-      open.value = false
-    }
-  }]
-}], [{
-  label: 'Feedback',
-  icon: 'i-lucide-message-circle',
-  to: 'https://github.com/nuxt-ui-templates/dashboard',
-  target: '_blank'
-}, {
-  label: 'Help & Support',
-  icon: 'i-lucide-info',
-  to: 'https://github.com/nuxt-ui-templates/dashboard',
-  target: '_blank'
 }]] satisfies NavigationMenuItem[][]
 
 const groups = computed(() => [{
@@ -89,35 +34,35 @@ const groups = computed(() => [{
     id: 'source',
     label: 'View page source',
     icon: 'i-simple-icons-github',
-    to: `https://github.com/nuxt-ui-templates/dashboard/blob/main/app/pages${route.path === '/' ? '/index' : route.path}.vue`,
+    to: 'https://github.com/UrayMR/ujikom-2026-client',
     target: '_blank'
   }]
 }])
 
-onMounted(async () => {
-  const cookie = useCookie('cookie-consent')
-  if (cookie.value === 'accepted') {
-    return
-  }
+// onMounted(async () => {
+//   const cookie = useCookie('cookie-consent')
+//   if (cookie.value === 'accepted') {
+//     return
+//   }
 
-  toast.add({
-    title: 'We use first-party cookies to enhance your experience on our website.',
-    duration: 0,
-    close: false,
-    actions: [{
-      label: 'Accept',
-      color: 'neutral',
-      variant: 'outline',
-      onClick: () => {
-        cookie.value = 'accepted'
-      }
-    }, {
-      label: 'Opt out',
-      color: 'neutral',
-      variant: 'ghost'
-    }]
-  })
-})
+//   toast.add({
+//     title: 'We use first-party cookies to enhance your experience on our website.',
+//     duration: 0,
+//     close: false,
+//     actions: [{
+//       label: 'Accept',
+//       color: 'neutral',
+//       variant: 'outline',
+//       onClick: () => {
+//         cookie.value = 'accepted'
+//       }
+//     }, {
+//       label: 'Opt out',
+//       color: 'neutral',
+//       variant: 'ghost'
+//     }]
+//   })
+// })
 </script>
 
 <template>
